de49f2e00f470010cead42f6934e6c29cc6ffa91
Ярослав Аверин <averinj@mail.ru>
2024-06-28 01:13:38 +0300

    init project with base settings

---
+++ ptk/urls.py
@@ -0,0 +1,16 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
+admin.site.site_title = "Администрирование PTK-сварка"
+admin.site.site_header = "Администрирование PTK-сварка"
+
+urlpatterns = [
+    path("__reload__/", include("django_browser_reload.urls")),
+    path('admin/', admin.site.urls),
+    path('', include('landing.urls')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
